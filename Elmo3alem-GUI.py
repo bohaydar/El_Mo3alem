330305d92c391b741f064c0a75e18edc77162db4
bo_haydar <139859144+bohaydar@users.noreply.github.com>
2023-08-02 18:20:08 +0300

    Update Elmo3alem-GUI.py

--- Elmo3alem-GUI.py
+++ Elmo3alem-GUI.py
@@ -5,7 +5,7 @@ s = """
    \033[1;32m  ______ _   __  __      ____        _
    \033[1;36m |  ____| | |  \/  |    |___ \      | |
    \033[1;33m | |__  | | | \  / | ___  __) | __ _| | ___ _ __ ___
-   \033[0;35m |  __| | | | |\/| |/ _ \|__ < / _` | |/ _ \ '_ ` _ \ 
+   \033[0;35m |  __| | | | |\/| |/ _ \|__ < / _` | |/ _ \ '_ ` _ \
    \033[1;31m | |____| | | |  | | (_) |__) | (_| | |  __/ | | | | |
    \033[1;32m |______|_| |_|  |_|\___/____/ \__,_|_|\___|_| |_| |_|\033[1;35m
 \033[0;32m
@@ -15,12 +15,12 @@ s = """
     |      INSTALL THE TERMUX SYSTEM GUI IN VNC VIWER     |
     |_____________________________________________________|
 \033[1;31m
-              Type Passwrd : [ 123456 ]
+                   Type Passwrd : [ 123456 ]
 \033[0;32m
      _____________________________________________________
     |_____________________________________________________|
 \033[1;36m
-                       [1]==> INSTQLL APP
+                       [1]==> INSTALL APP
 
                        [2]==> INSTALL GUI
 
@@ -31,7 +31,7 @@ s = """
 
 """
 for i in s:
-     time.sleep(0.02)
+     time.sleep(0.01)
      sys.stdout.write(i)
      sys.stdout.flush()
 vvvv = input('''\033[1;34m
@@ -39,7 +39,7 @@ vvvv = input('''\033[1;34m
 
             Enter Number\033[1;35m ==> √  \033[1;36m:    ''')
 if vvvv =='' :
-   os.system("python elmo3alem.py")
+   os.system("python Elmo3alem-GUI.py")
 elif vvvv =='1' :
      print ("\033[1;31mINSTALL THE APP VNC VEWER IN MY FILES THEN INSTALL IT")
      os.system("cp APP.apk /sdcard")
@@ -71,3 +71,25 @@ Go To Vnv Vewar Click + type [\033[1;32m localhost:1\033[1;31m ]
 type name [\033[1;32m Termux\033[1;31m ] What name there is no difference
 ENTER PASSWRD [\033[1;32m 123456\033[1;31m ]
 """)
+elif vvvv =='3' :
+     os.system("clear")
+     s2 = """\033[1;35m
+                         _____       __
+                        |_   _|     / _|
+                          | | _ __ | |_ ___
+                          | || '_ \|  _/ _ \
+                         _| || | | | || (_) |
+                         \___/_| |_|_| \___/\033[1;36m
+         The El mo3alem tool For Noob Hackers '_' :)
+                 By Bo Haydar El Mo3alem
+\033[1;35m
+         ___________________________________________
+        |Youtube:https://youtube.com/@BO_HAYDAR_HK  |
+        |Tele:https://t.me/txrtm                    |
+        |Tele Team:https://t.me/+EHo65POl1yA2ZjA0   |
+        |___________________________________________|
+"""
+for i in s2:
+     time.sleep(0.01)
+     sys.stdout.write(i)
+     sys.stdout.flush()
